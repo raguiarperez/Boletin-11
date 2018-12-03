4d4c12c08aa3336f0b012b7f502d6a784b1a1c7b
raguairperez <rafa2@LAPTOP-SPV2MGHO>
2018-12-03 11:20:38 +0100

    corregido

--- src/boletin11/Instancia.java
+++ src/boletin11/Instancia.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package boletin11;
 
 import java.text.DecimalFormat;
 import javax.swing.JOptionPane;
 
 /**
- *
  * @author rafaguiarp
  */
 public class Instancia {
@@ -21,11 +15,11 @@ public class Instancia {
         long inicio, fin;
         String frase = "A documentación é todo aquel conxunto de manuais impresos ou en formato dixital que explique unha aplicación informática.";
         inicio = System.nanoTime();
-        JOptionPane.showInputDialog(frase, "ESCRIBE LA SIGUIENTE FRASE" + "\n A documentación é todo aquel conxunto de manuais impresos ou en formato dixital que explique unha aplicación informática.");
+        String Escrito= JOptionPane.showInputDialog(frase, "ESCRIBE LA SIGUIENTE FRASE" + "\n A documentación é todo aquel conxunto de manuais impresos ou en formato dixital que explique unha aplicación informática.");
         fin = System.nanoTime();
         float total = fin - inicio;
         DecimalFormat df = new DecimalFormat("#.00");
+        if(frase.equals(Escrito)==true);
         JOptionPane.showMessageDialog(null, "Tardaches " + (df.format(total / 1e9)) + " segundos en escribir a frase");
-
     }
 }
